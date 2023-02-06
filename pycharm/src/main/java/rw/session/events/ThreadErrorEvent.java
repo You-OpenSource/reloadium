c07f0dc11b06c00e12d9cda62ef58905b41cd766
dmi3coder <dmi3coder@gmail.com>
2023-02-06 21:55:37 +0100

    chore: update to reloadium 0.9.10

--- pycharm/src/main/java/rw/session/events/ThreadErrorEvent.java
+++ pycharm/src/main/java/rw/session/events/ThreadErrorEvent.java
@@ -1,8 +1,15 @@
 package rw.session.events;
 
 import com.google.gson.annotations.SerializedName;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileWrapper;
 import rw.dialogs.DialogFactory;
+import rw.service.CodeCompletionService;
 
 public class ThreadErrorEvent extends FileError {
     private static final Logger LOGGER = Logger.getInstance(ThreadErrorEvent.class);
@@ -20,6 +27,14 @@ public class ThreadErrorEvent extends FileError {
         LOGGER.info("Handling ThreadErrorEvent");
         this.handler.getStack().onThreadError(this);
         DialogFactory.get().showFirstThreadErrorDialog(this.handler.getProject());
+        // todo still broken
+        VirtualFile file = new VirtualFileWrapper(this.getLocalPath()).getVirtualFile(false);
+        Document document = ReadAction.compute(() -> FileDocumentManager.getInstance().getDocument(file));
+        int lineNumber = getLine() - 1;
+        String errorLine = document.getText(TextRange.create(document.getLineStartOffset(lineNumber), document.getLineEndOffset(lineNumber)));
+        String res = CodeCompletionService.INSTANCE.predictFix(errorLine).blockingFirst();
+        this.handler.getSolutionHighlightManager().clearAll();
+        this.handler.getSolutionHighlightManager().add(this.getLocalPath(), this.getLine(), this.getMsg(), res);
     }
 
     public String getThreadId() {
